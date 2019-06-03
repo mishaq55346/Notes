2f66267490ab25b312f8ff31f28de965a919bb00
Mikhail <20598587+mishaq55346@users.noreply.github.com>
2019-06-03 21:38:21 +0300

    bug fixes

--- Notes/main.cpp
+++ Notes/main.cpp
@@ -15,10 +15,13 @@ int main()
 	setlocale(LC_ALL, "Russian");
 	SetConsoleCP(1251);
 	SetConsoleOutputCP(1251);
+	printf("Что вы хотите сделать?\n EXIT - Выход из программы\n ADD - Добавить событие\n PRINT - Распечатать все события для данной даты\n EDIT - Редактировать события для данной даты\n DELETE - Удалить событие для данной даты\n");
 	string choice;
 	cin >> choice;
 	while (choice != "EXIT") {
 		handleChoice(choice);
+		printf("EXIT - Выход из программы\n ADD - Добавить событие\n PRINT - Распечатать все события для данной даты\n EDIT - Редактировать события для данной даты\n DELETE - Удалить событие для данной даты\n");
+		cin >> choice;
 	}
 	system("pause");
 	return 0;
@@ -28,9 +31,11 @@ void handleChoice(string choice)
 {
 	if(choice == "ADD")
 	{
+		printf("Введите через пробел день, месяц, год(4х-значный) и событие\n");
 		int day, month, year;
 		string event;
-		cin >> day >> month >> year >> event;
+		cin >> day >> month >> year;
+		getline(cin, event);
 		notes.add(day, month, year, event);
 	}
 	if(choice == "EDIT")
@@ -39,6 +44,7 @@ void handleChoice(string choice)
 	}
 	if (choice == "PRINT")
 	{
+		printf("Введите через пробел день, месяц и год(4х-значный)\n");
 		int day, month, year;
 		cin >> day >> month >> year;
 		notes.print(day, month, year);
